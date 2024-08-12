92bc76b2110f8272c0107484434321014a7b3ccd
Nourhan Hasan <Nourhan.M.Hasan@gmail.com>
2024-08-12 10:19:52 +0300

    feat: dockerize MySQL

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,29 @@
 import express from 'express';
 import dotenv from 'dotenv';
+import mysql from 'mysql2';
 
-const app = express();
 dotenv.config();
+const app = express();
 
-app.listen(process.env.PORT || 3000, () => {
-    console.log(`Server running on port: ${process.env.PORT}`);
+const db = mysql.createConnection({
+    host: process.env.DB_HOST || 'db',
+    user: process.env.DB_USER || 'root',
+    password: process.env.MYSQL_ROOT_PASSWORD || 'example',
+    database: process.env.MYSQL_DATABASE || 'smart-pharmacy-db'
+});
+
+db.connect((err) => {
+    if (err) {
+        console.error('Error connecting to the database:', err);
+        return;
+    }
+    console.log('Connected to the MySQL database.');
+});
+
+app.get('/', (req, res) => {
+    res.send('Hello, World!');
+});
+
+app.listen(3000, () => {
+    console.log(`Server running on port: ${3000}`);
 });
